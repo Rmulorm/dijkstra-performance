7b465cd7e8624edd7edd04acb8098985a0dbcef3
Rômulo Maciel <romulo.maciel@sap.com>
2021-11-21 16:30:18 -0300

    moved place of output data

---
+++ output_data/README.md
@@ -0,0 +1,1 @@
+This folder contains the output data of the performance tests.
--- src/main/java/com/shortestpath/main/ProjectConstants.java
+++ src/main/java/com/shortestpath/main/ProjectConstants.java
@@ -1,10 +1,10 @@
 package com.shortestpath.main;
 
 public final class ProjectConstants {
-  public static final String BRUTE_FORCE_FILE_NAME = "brute_force_output_data.csv";
-  public static final String BASE_DIJKSTRA_FILE_NAME = "base_dijkstra_output_data.csv";
+  public static final String BRUTE_FORCE_FILE_NAME = "output_data/brute_force_output_data.csv";
+  public static final String BASE_DIJKSTRA_FILE_NAME = "output_data/base_dijkstra_output_data.csv";
   public static final String PRIORITY_QUEUE_DIJKSTRA_FILE_NAME =
-      "priority_queue_dijkstra_output_data.csv";
+      "output_data/priority_queue_dijkstra_output_data.csv";
 
   private ProjectConstants() {}
 }
